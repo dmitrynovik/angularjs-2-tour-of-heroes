b6bc58c0a76cfd50c1812414944de59fed8bfe16
dmitrynovik <cryinstone@gmail.com>
2017-03-27 13:32:27 +1100

    completed hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { Hero }
+import { Hero } from './hero'
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
 export class AppComponent  { 
   name = 'Angular'; 

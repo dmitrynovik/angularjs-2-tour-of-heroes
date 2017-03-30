91459e5ef2bbdefdda4edaddf0af2bfbf7ab7290
dmitrynovik <cryinstone@gmail.com>
2017-03-30 13:04:21 +1100

    navigation to details

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
-
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html'
@@ -15,7 +14,11 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
+      .then(
+          heroes => { this.heroes = heroes.slice(1, 5);
+              console.log("heroes on dashboard:", this.heroes.length);
+          }
+      );
   }
 }
 
